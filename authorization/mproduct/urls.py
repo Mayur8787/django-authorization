b6c6dd3343f8f870698ee38f894f0d2c1e5a9585
Mayur8787 <mayurgund8773@gmail.com>
2024-09-09 22:44:07 +0530

    Susbscriptions and subscribers management functionality added

--- authorization/mproduct/urls.py
+++ authorization/mproduct/urls.py
@@ -5,15 +5,18 @@ from mproduct.views import *
 app_name = "mproduct"
 
 urlpatterns = [
+    path("",HomeView.as_view(),name='home'),
     path("login/",LoginView.as_view(template_name='mproduct/login.html'),name='login'),
     path("logout/",LogoutView.as_view(),name="logout"),
     path("register/",RegisterView.as_view(),name="register"),
     path("settings/",SettingsView.as_view(),name='settings'),
     path("create/brand/",CreateBrand.as_view(),name='create_brand'),
     path("create/plan/",CreatePlan.as_view(),name='create_plan'),
-    path("",HomeView.as_view(),name='home'),
+    path("subscription/",SubscriptionView.as_view(),name='subscription'),
+    path("subscribers/",SubscribersView.as_view(),name='subscribers'),
+    path("subscription/status/<int:pk>/",SubscriptionStatusView.as_view(),name='status'),
     path("plans/",PlansView.as_view(),name='plans'),
-    path("planss/",PurchaseView.as_view(),name='purchase'),
+    path("plans/<int:pk>/",BrandPlansView.as_view(),name='brand_plans'),
     path("purchase/",SubscribeView.as_view(),name='new_purchase'),
     path("search/",search_brands,name='search_brands')
 ]
--- authorization/mproduct/views.py
+++ authorization/mproduct/views.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 from django.contrib.auth.mixins import LoginRequiredMixin
 from mproduct.forms import UserRegisterForm
 from django.contrib.auth.models import User
-from django.views.generic import CreateView, TemplateView, ListView, DetailView, View
+from django.views.generic import CreateView, TemplateView, ListView, DetailView,UpdateView, View
 from mproduct.models import Plans, Brand, Subscribers
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_GET
@@ -25,7 +25,10 @@ class HomeView(TemplateView):
     def get_context_data(self, **kwargs: reverse_lazy):
         context = super().get_context_data(**kwargs)
         context['is_authenticated'] = self.request.user.is_authenticated
+        context['is_authorized'] = Subscribers.objects.filter(name=self.request.user,subscription_status=True).exists() if context['is_authenticated'] else False
+        context['is_brand'] = Brand.objects.filter(owner=self.request.user).exists() if context['is_authorized'] else False
         context['title'] = 'Home'
+        print("---------->",context)
         return context
 
 class PlansView(LoginRequiredMixin,ListView):
@@ -41,7 +44,19 @@ class PlansView(LoginRequiredMixin,ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['is_authenticated'] = self.request.user.is_authenticated
+        context['title'] = 'Plans'
         return context
+
+class BrandPlansView(LoginRequiredMixin,ListView):
+    model = Plans
+    template_name = 'mproduct/plans.html'
+    context_object_name = 'plans'
+    login_url = 'mproduct:login'
+
+    def get_queryset(self):
+        brand = Brand.objects.get(pk=self.kwargs.get('pk'))
+        queryset = super().get_queryset()
+        return queryset.filter(brand=brand)
 
 class SubscribeView(LoginRequiredMixin,View):
     login_url = 'mproduct:login'
@@ -88,9 +103,10 @@ class SettingsView(LoginRequiredMixin,TemplateView):
         context = super().get_context_data(**kwargs)
         context['is_authenticated'] = self.request.user.is_authenticated
         context['profile'] = get_object_or_404(User,username=self.request.user)
-        context['is_authorized'] = False if not Subscribers.objects.filter(name=self.request.user).first() else True
+        context['is_authorized'] = Subscribers.objects.filter(name=self.request.user,subscription_status=True).exists()
+        context['title'] = 'Settings'
         if context['is_authorized']:
-            context['is_brand'] = False if not Brand.objects.filter(owner=self.request.user).first() else True
+            context['is_brand'] = Brand.objects.filter(owner=self.request.user).exists()
         else:
             context['is_brand'] = False
         if context['is_brand']:
@@ -106,51 +122,58 @@ class SettingsView(LoginRequiredMixin,TemplateView):
         return context
 
 
-class PurchaseView(LoginRequiredMixin,ListView):
-    model = Plans
-    template_name = 'mproduct/purchase.html'
-    context_object_name = 'plans'
-    login_url = 'mproduct:login'
-
-
-class SubscriptionView(LoginRequiredMixin,DetailView):
+class SubscriptionView(LoginRequiredMixin,ListView):
+    model = Subscribers
     template_name = 'mproduct/subscription.html'
-    context_object_name = 'subscription'
+    context_object_name = 'subscriptions'
     login_url = 'mproduct:login'
 
+    def get_queryset(self) -> QuerySet[reverse_lazy]:
+        queryset = super().get_queryset()
+        return queryset.filter(name=self.request.user)
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['subscribers']
-
-@login_required(login_url='mproduct:login')
-def purchase(request):
-    context = {}
-    context['is_authenticated'] = True
-    if request.method == 'POST':
-        plan_id = request.POST.get('plan_id')
-        plan = get_object_or_404(Plans,id=plan_id)
-        name = request.user
-        date = timezone.now().date()
-        status = True
-        subscriber = Subscribers.objects.create(name=name,plan=plan,purchased_on=date,subscription_status=status)
-        return redirect('mproduct:home')
-    else:
-        brand = Brand.objects.filter(name='mproduct')
-        if brand:
-            brand = brand[0]
-            context['plans'] = Plans.objects.filter(brand=brand)
-        else:
-            context['plans'] = []
-    return render(request=request,template_name='mproduct/purchase.html',context=context)
-
-class LandingPageView(TemplateView):
-    template_name = "mproduct/landing.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'base'
         context['is_authenticated'] = self.request.user.is_authenticated
+        context['is_authorized'] = Subscribers.objects.filter(name=self.request.user,subscription_status=True).exists()
+        context['is_brand'] = Brand.objects.filter(owner=self.request.user).exists()
+        context['title'] = 'Subscriptions'
         return context
+
+class SubscribersView(LoginRequiredMixin,ListView):
+    model = Subscribers
+    template_name = 'mproduct/subscribers.html'
+    context_object_name = 'subscribers'
+    login_url = 'mproduct:login'
+
+    def get_queryset(self) -> QuerySet[reverse_lazy]:
+        queryset = super().get_queryset()
+        if Brand.objects.filter(owner=self.request.user).exists():
+            brand = Brand.objects.get(owner=self.request.user)
+            return queryset.filter(plan__brand=brand)
+        return queryset.none()
+    
+    def get_context_data(self, **kwargs: reverse_lazy) :
+        context = super().get_context_data(**kwargs)
+        context['is_authenticated'] = self.request.user.is_authenticated
+        context['is_authorized'] = Subscribers.objects.filter(name=self.request.user,subscription_status=True).exists()
+        context['is_brand'] = Brand.objects.filter(owner=self.request.user).exists()
+        context['title'] = 'Subscribers'
+        return context
+
+class SubscriptionStatusView(LoginRequiredMixin,UpdateView):
+    login_url = 'mproduct:login'
+    model = Subscribers
+    fields = []
+    success_url = reverse_lazy('mproduct:subscribers')
+
+    def form_valid(self, form):
+        subscriber = form.instance
+        end_date = subscriber.purchased_on + timezone.timedelta(days=subscriber.plan.duration)
+        if end_date > timezone.now().date():
+            subscriber.subscription_status = not subscriber.subscription_status
+        subscriber.save()
+        return super().form_valid(form)
 
 @require_GET
 def search_brands(request):
